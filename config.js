3e96767c9bf5e1a0fc1d67831997180cea5fb93d
widczon <skhyunkim@gmail.com>
2025-12-06 13:04:43 +0700

    Ngrok url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_URL: "https://lashonda-knarry-tayna.ngrok-free.dev",  
-    MEDIA_URL: "https://lashonda-knarry-tayna.ngrok-free.dev" 
+    API_URL: " https://lashonda-knarry-tayna.ngrok-free.dev",  
+    MEDIA_URL: " https://lashonda-knarry-tayna.ngrok-free.dev" 
 };
